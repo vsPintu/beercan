a2b2036cbf553d0b1191418965850937b75c6da2
Pintoo <pc8307291858@gmail.com>
2024-07-09 11:06:17 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Navbar from "./components/common/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-     App
-    </>
-  )
+    <div className="max-w-[1280px] mx-auto">
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cararra: "#f0f0ea",
+        supernova: "#ffcc02",
+        silver: "#cccccc",
+      },
+      fontFamily: {
+        poppins: "poppins",
+        inter: "inter",
+        roboto: "roboto",
+        play: "play",
+      },
+    },
   },
   plugins: [],
-}
-
+};
