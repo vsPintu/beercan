0fc94c5036387898f8fb7f5b81612dd5928c48e0
Code-Wide <codewide.hisar.com>
2024-07-15 11:48:59 +0530

    aaaaaaaa

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,11 @@ import RebelCardSlider from "./components/homepage/RebelCardSlider";
 import Header from "./components/homepage/Header";
 import FreeShipping from "./components/common/FreeShipping";
 import SideBar from "./components/common/SideBar";
-<<<<<<< HEAD
-import HeroSlider from "./components/common/HeroSlider";
-import OffersAndPackage from "./components/homepage/OffersAndPackage";
-
-function App() {
-  return (
-    <div className="max-w-[1440px] mx-auto">
-=======
 import Footer from "./components/common/Footer";
 
 function App() {
   return (
     <div className="max-w-[1440px] app mx-auto">
->>>>>>> f16ca9ebce58068dafe8eda80cb034de398599c5
       <SideBar />
       <FreeShipping />
       <Header />
