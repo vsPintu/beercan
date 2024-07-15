a92c7eb67085b97461f2d1f83b824d4b328a17a3
Code-Wide <codewide.hisar.com>
2024-07-15 12:08:15 +0530

    code merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import RebelCardSlider from "./components/homepage/RebelCardSlider";
-import Header from "./components/homepage/Header";
-import FreeShipping from "./components/common/FreeShipping";
 import SideBar from "./components/common/SideBar";
 import Footer from "./components/common/Footer";
+import Header from "./components/homepage/Header";
+import RebelCardSlider from "./components/homepage/RebelCardSlider";
+import FreeShipping from "./components/common/FreeShipping";
 
 function App() {
   return (
-    <div className="max-w-[1440px] app mx-auto">
-      <SideBar />
+    <div>
       <FreeShipping />
       <Header />
-      <HeroSlider />
-      <OffersAndPackage />
       <RebelCardSlider />
       <Footer />
     </div>
